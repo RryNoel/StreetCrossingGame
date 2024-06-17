8acd6677d99c34f6bf7e47b24e361cc708825e88
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-06-17 17:34:01 +0900

    Feature : 도로 생성 로직 구현

--- Assets/Scripts/Manager/EnvironmentManager.cs
+++ Assets/Scripts/Manager/EnvironmentManager.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvironmentManager : MonoBehaviour
+public class EnvironmentManager : Singleton<EnvironmentManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] EnvironmentObjectArray;
+    public Transform ParentTransform;
+
+    public int MinPosZ = -20;
+    public int MaxPosZ = 20;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for(int i = MinPosZ; i < MaxPosZ; ++i)
+        {
+            CloneRoad(i);
+        }
+    }
+
+    void CloneRoad(int posZ)
+    {
+        int randomindex = Random.Range(0, EnvironmentObjectArray.Length);
+        GameObject cloneObj = Instantiate(EnvironmentObjectArray[randomindex]);
+
+        Vector3 offSetPos = Vector3.zero;
+        offSetPos.z = (float)posZ;
+        cloneObj.transform.SetParent(ParentTransform);
+        cloneObj.transform.position = offSetPos;
+
+        int randomRot = Random.Range(0, 2);
+        if (randomRot == 1)
+        {
+            cloneObj.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
     }
 }
